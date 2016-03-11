6e1e9af48c873f8b757ce7e8ddc47d9bcfba9b52
speace0151 <samanthapeace23@gmail.com>
2016-03-11 11:21:07 -0600

    hi

--- src/troubleingolddust/Troubleingolddust.java
+++ src/troubleingolddust/Troubleingolddust.java
@@ -46,26 +46,26 @@ import org.newdawn.slick.state.transition.FadeInTransition;
 import org.newdawn.slick.state.transition.FadeOutTransition;
 
 import org.newdawn.slick.tiled.TiledMap;
-import org.w3c.dom.css.Rect;
 
 public class Troubleingolddust extends BasicGameState {
-
+    
     public Item healthpotion, healthpotion1;
     public Item1 speedpotion, speedpotion1;
     public Itemwin gold;
     public Orb orb;
     public Enemy wrangler, wrangler1;
     public Orb magic8ball;
-    public ArrayList<Orb> orbz =  new ArrayList();
+    public ArrayList<Orb> orbz = new ArrayList();
     public ArrayList<Enemy> enemiez = new ArrayList();
     public ArrayList<Item> stuff = new ArrayList();
-
+    public Trap traps;
     public ArrayList<Item1> stuff1 = new ArrayList();
-
+    
+public ArrayList<Item> trapz = new ArrayList();
     public ArrayList<Itemwin> stuffwin = new ArrayList();
 
     private boolean[][] hostiles;
-
+    public static Player player;
     boolean isvisible = true;
     private static TiledMap grassMap;
 
@@ -74,10 +74,9 @@ public class Troubleingolddust extends BasicGameState {
     private static Camera camera;
 
     public static int counter = 0;
-
-    // Player stuff
-    private Animation sprite, up, down, left, right, wait;
-
+    
+    // player stuff
+    // private Animation sprite, up, down, left, right, wait;
     /**
      *
      * The collision map indicating which tiles block movement - generated based
@@ -91,19 +90,15 @@ public class Troubleingolddust extends BasicGameState {
     private static final int SCREEN_WIDTH = 1000;
 
     private static final int SCREEN_HEIGHT = 750;
-    private Animation attack;
 
+    //  private Animation attack;
+// public static Player player;
     public Troubleingolddust(int xSize, int ySize) {
 
     }
 
     public void init(GameContainer gc, StateBasedGame sbg)
             throws SlickException {
-orb = new Orb((int)Player.x, (int) Player.y);
-if(orb.isIsVisible()){
-    orb.orbpic.draw(orb.getX(), orb.getY());
-}
-
 
         gc.setTargetFrameRate(60);
 
@@ -120,125 +115,12 @@ if(orb.isIsVisible()){
         camera = new Camera(gc, grassMap);
 
         // *********************************************************************************
-        // Player stuff --- these things should probably be chunked into methods
+        // player stuff --- these things should probably be chunked into methods
         // and classes
         // *********************************************************************************
-        SpriteSheet runningSS = new SpriteSheet(
-                "res/protagonist.png", 64, 64, 0);
-
         // System.out.println("Horizontal count: "
         // +runningSS.getHorizontalCount());
         // System.out.println("Vertical count: " +runningSS.getVerticalCount());
-        attack = new Animation();
-        attack.setAutoUpdate(true);
-        
-        for (Enemy e:  enemiez)
-        up = new Animation();
-
-        up.setAutoUpdate(true);
-
-        up.addFrame(runningSS.getSprite(0, 8), 330);
-
-        up.addFrame(runningSS.getSprite(1, 8), 330);
-
-        up.addFrame(runningSS.getSprite(2, 8), 330);
-
-        up.addFrame(runningSS.getSprite(3, 8), 330);
-
-        up.addFrame(runningSS.getSprite(4, 8), 330);
-
-        up.addFrame(runningSS.getSprite(5, 8), 330);
-
-        up.addFrame(runningSS.getSprite(6, 8), 330);
-
-        up.addFrame(runningSS.getSprite(7, 8), 330);
-
-        up.addFrame(runningSS.getSprite(8, 8), 330);
-
-        down = new Animation();
-
-        down.setAutoUpdate(false);
-
-        down.addFrame(runningSS.getSprite(0, 10), 330);
-
-        down.addFrame(runningSS.getSprite(1, 10), 330);
-
-        down.addFrame(runningSS.getSprite(2, 10), 330);
-
-        down.addFrame(runningSS.getSprite(3, 10), 330);
-
-        down.addFrame(runningSS.getSprite(4, 10), 330);
-
-        down.addFrame(runningSS.getSprite(5, 10), 330);
-
-        down.addFrame(runningSS.getSprite(6, 10), 330);
-
-        down.addFrame(runningSS.getSprite(7, 10), 330);
-
-        down.addFrame(runningSS.getSprite(8, 10), 330);
-
-        left = new Animation();
-
-        left.setAutoUpdate(false);
-
-        left.addFrame(runningSS.getSprite(0, 9), 330);
-
-        left.addFrame(runningSS.getSprite(1, 9), 330);
-
-        left.addFrame(runningSS.getSprite(2, 9), 330);
-
-        left.addFrame(runningSS.getSprite(3, 9), 330);
-
-        left.addFrame(runningSS.getSprite(4, 9), 330);
-
-        left.addFrame(runningSS.getSprite(5, 9), 330);
-
-        left.addFrame(runningSS.getSprite(6, 9), 330);
-
-        left.addFrame(runningSS.getSprite(7, 9), 330);
-
-        left.addFrame(runningSS.getSprite(8, 9), 330);
-
-        right = new Animation();
-
-        right.setAutoUpdate(false);
-
-        right.addFrame(runningSS.getSprite(0, 11), 330);
-
-        right.addFrame(runningSS.getSprite(1, 11), 330);
-
-        right.addFrame(runningSS.getSprite(2, 11), 330);
-
-        right.addFrame(runningSS.getSprite(3, 11), 330);
-
-        right.addFrame(runningSS.getSprite(4, 11), 330);
-
-        right.addFrame(runningSS.getSprite(5, 11), 330);
-
-        right.addFrame(runningSS.getSprite(6, 11), 330);
-
-        right.addFrame(runningSS.getSprite(7, 11), 330);
-
-        right.addFrame(runningSS.getSprite(8, 11), 330);
-
-        wait = new Animation();
-
-        wait.setAutoUpdate(true);
-
-        wait.addFrame(runningSS.getSprite(0, 14), 733);
-
-        wait.addFrame(runningSS.getSprite(1, 14), 733);
-
-        wait.addFrame(runningSS.getSprite(2, 14), 733);
-
-        wait.addFrame(runningSS.getSprite(3, 14), 733);
-if(orb.isIsVisible()){
-    
-}
-        // wait.addFrame(runningSS.getSprite(2, 14), 733);
-        // wait.addFrame(runningSS.getSprite(5, 14), 333);
-        sprite = wait;
-
         // *****************************************************************
         // Obstacles etc.
         // build a collision map based on tile properties in the TileD map
@@ -251,9 +133,6 @@ if(orb.isIsVisible()){
         // You could also use this for planning traps, etc.
         // System.out.println("Number of tile layers: "
         // +grassMap.getLayerCount());
-        System.out.println("The grassmap is " + grassMap.getWidth() + "by "
-                + grassMap.getHeight());
-
         for (int xAxis = 0; xAxis < grassMap.getWidth(); xAxis++) {
 
             for (int yAxis = 0; yAxis < grassMap.getHeight(); yAxis++) {
@@ -269,23 +148,15 @@ if(orb.isIsVisible()){
 
                 if ("true".equals(value)) {
 
-                    System.out.println("The tile at x " + xAxis + " andy axis "
-                            + yAxis + " is blocked.");
-
                     Blocked.blocked[xAxis][yAxis] = true;
 
                 }
 
             }
-         
 
         }
 
-        System.out.println("Array length" + Blocked.blocked[0].length);
-
         // A remarkably similar process for finding hostiles
-        hostiles = new boolean[grassMap.getWidth()][grassMap.getHeight()];
-
         for (int xAxis = 0; xAxis < grassMap.getWidth(); xAxis++) {
             for (int yAxis = 0; yAxis < grassMap.getHeight(); yAxis++) {
                 int xBlock = (int) xAxis;
@@ -295,7 +166,7 @@ if(orb.isIsVisible()){
                         Item i = new Item(xAxis * SIZE, yAxis * SIZE);
                         stuff.add(i);
                         //stuff1.add(h);
-                        hostiles[xAxis][yAxis] = true;
+
                     }
                 }
             }
@@ -310,11 +181,13 @@ if(orb.isIsVisible()){
                         Item1 h = new Item1(xAxis * SIZE, yAxis * SIZE);
                         //	stuff.add(i);
                         stuff1.add(h);
-                        hostiles[xAxis][yAxis] = true;
+
                     }
                 }
             }
         }
+        player = new Player();
+        orb = new Orb((int) player.x, (int) player.y);
 
         healthpotion = new Item(100, 100);
         healthpotion1 = new Item(450, 480);
@@ -326,21 +199,20 @@ if(orb.isIsVisible()){
         stuff1.add(speedpotion);
         stuff1.add(speedpotion1);
 
-        gold = new Itemwin(2474,829 );
+        gold = new Itemwin(2474, 829);
         stuffwin.add(gold);
         wrangler = new Enemy(200, 300);
-        wrangler1 = new Enemy (96,1261);
+        wrangler1 = new Enemy(96, 1261);
         enemiez.add(wrangler);
         enemiez.add(wrangler1);
-
-        
-
+//traps = new Trap(200, 200);
+//traps.add(traps);
     }
 
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
             throws SlickException {
 
-        camera.centerOn((int) Player.x, (int) Player.y);
+        camera.centerOn((int) player.x, (int) player.y);
 
         camera.drawMap();
 
@@ -349,22 +221,24 @@ if(orb.isIsVisible()){
         // it helps to add status reports to see what's going on
         // but it gets old quickly
         // System.out.println("Current X: " +player.x + " \n Current Y: "+ y);
-        sprite.draw((int) Player.x, (int) Player.y);
+        player.sprite.draw((int) player.x, (int) player.y);
 
-        g.drawString("x: " + (int)Player.x + " y: " +(int)Player.y , Player.x, Player.y - 10);
-        
-        g.drawString("Health: " + Player.health / 1000, camera.cameraX + 10,
+        //g.drawString("x: " + (int) player.x + " y: " + (int) player.y, player.x, player.y - 10);
+        g.drawString("Health: " + player.health / 1000, camera.cameraX + 10,
                 camera.cameraY + 10);
 
-        g.drawString("speed: " + (int) (Player.speed * 10), camera.cameraX + 10,
+        g.drawString("speed: " + (int) (player.speed * 10), camera.cameraX + 10,
                 camera.cameraY + 25);
+        
+        g.drawString("score: " + (int) (player.score * 15), camera.cameraX + 15,
+                camera.cameraY + 40);
 
         //g.draw(player.rect);
-        g.drawString("time passed: " + counter / 1000, camera.cameraX + 600, camera.cameraY);
+        //g.drawString("time passed: " + counter / 1000, camera.cameraX + 600, camera.cameraY);
         // moveenemies();
-        
-        if(gold.isvisible){
-        gold.currentImage.draw(gold.x, gold.y);
+
+        if (gold.isvisible) {
+            gold.currentImage.draw(gold.x, gold.y);
         }
         for (Item i : stuff) {
             if (i.isvisible) {
@@ -373,11 +247,8 @@ if(orb.isIsVisible()){
                 //g.draw(i.hitbox);
 
             }
-                            
-            }
-     
-        
-        
+
+        }
 
         for (Item1 h : stuff1) {
             if (h.isvisible) {
@@ -406,142 +277,160 @@ if(orb.isIsVisible()){
         wrangler.move();
         Input input = gc.getInput();
 
-        float fdelta = delta * Player.speed;
+        float fdelta = delta * player.speed;
 
-        Player.setpdelta(fdelta);
+        player.setpdelta(fdelta);
 
         double rightlimit = (grassMap.getWidth() * SIZE) - (SIZE * 0.75);
 
         // System.out.println("Right limit: " + rightlimit);
-        float projectedright = Player.x + fdelta + SIZE;
+        float projectedright = player.x + fdelta + SIZE;
 
         boolean cangoright = projectedright < rightlimit;
 
         // there are two types of fixes. A kludge and a hack. This is a kludge.
         if (input.isKeyDown(Input.KEY_UP)) {
 
-            sprite = up;
+            player.sprite = player.up;
 
             float fdsc = (float) (fdelta - (SIZE * .15));
 
-            if (!(isBlocked(Player.x, Player.y - fdelta) || isBlocked((float) (Player.x + SIZE + 1.5), Player.y - fdelta))) {
+            if (!(isBlocked(player.x, player.y - fdelta) || isBlocked((float) (player.x + SIZE + 1.5), player.y - fdelta))) {
 
-                sprite.update(delta);
+                player.sprite.update(delta);
 
-               
-                Player.y -= fdelta;
+                player.y -= fdelta;
 
             }
+            for (Itemwin w : stuffwin) {
 
+            }
         } else if (input.isKeyDown(Input.KEY_DOWN)) {
-            sprite = down;
+            player.sprite = player.down;
 
-            if (!isBlocked(Player.x, Player.y + SIZE + fdelta)
-                    || !isBlocked(Player.x + SIZE - 1, Player.y + SIZE + fdelta)) {
+            if (!isBlocked(player.x, player.y + SIZE + fdelta)
+                    || !isBlocked(player.x + SIZE - 1, player.y + SIZE + fdelta)) {
 
-                sprite.update(delta);
+                player.sprite.update(delta);
                 {
-                Player.y += fdelta;
-                
-         
-            
+                    player.y += fdelta;
+
                 }
-            
-                
+
+            } else if (input.isKeyDown(Input.KEY_SPACE)) {
+                orb.setX((int) player.x);
+                orb.setY((int) player.y);
+                orb.hitbox.setX(orb.getX());
+                orb.hitbox.setY(orb.getY());
+                orb.setIsVisible(!orb.isIsVisible());
+                orb.setIsVisible(true);
             }
-           else if (input.isKeyDown(Input.KEY_SPACE)){
-                    orb.setX((int) Player.x);
-                    orb.setY((int) Player.y);
-                    orb.hitbox.setX(orb.getX());
-                    orb.hitbox.setY(orb.getY());
-                    orb.setIsVisible(!orb.isIsVisible());
-                    orb.setIsVisible(true);
-                    }
         } else if (input.isKeyDown(Input.KEY_LEFT)) {
-Player.setdirection = 2;
-            sprite = left;
+            player.setdirection = 2;
+            player.sprite = player.left;
 
-            if (!(isBlocked(Player.x - fdelta, Player.y) || isBlocked(Player.x
-                    - fdelta, Player.y + SIZE - 1))) {
+            if (!(isBlocked(player.x - fdelta, player.y) || isBlocked(player.x
+                    - fdelta, player.y + SIZE - 1))) {
 
-                sprite.update(delta);
+                player.sprite.update(delta);
 
-                Player.x -= fdelta;
+                player.x -= fdelta;
 
             }
 
         } else if (input.isKeyDown(Input.KEY_RIGHT)) {
 
-            sprite = right;
+            player.sprite = player.right;
 
             if (cangoright
-                    && (!(isBlocked(Player.x + SIZE + fdelta,
-                            Player.y) || isBlocked(Player.x + SIZE + fdelta, Player.y
+                    && (!(isBlocked(player.x + SIZE + fdelta,
+                            player.y) || isBlocked(player.x + SIZE + fdelta, player.y
                             + SIZE - 1)))) {
 
                 wrangler = new Enemy(2, 2);
-                sprite.update(delta);
+                player.sprite.update(delta);
 
-                Player.x += fdelta;
+                player.x += fdelta;
 
             }
 
         }
 
-        Player.rect.setLocation(Player.getplayershitboxX(),
-                Player.getplayershitboxY());
+        player.rect.setLocation(player.getplayershitboxX(),
+                player.getplayershitboxY());
 
         for (Item i : stuff) {
 
-            if (Player.rect.intersects(i.hitbox)) {
+            if (player.rect.intersects(i.hitbox)) {
 
                 if (i.isvisible) {
-
-                    Player.health += 10000;
+                    player.score += 7;
+                    player.health += 10000;
                     i.isvisible = false;
                 }
-                }
+                
+            }
         }
-for (Enemy e : enemiez){
-    if (orb.hitbox.intersects(e.rect)){
-        e.isvisible = false;
-  
+        for (Enemy e : enemiez) {
+            if (orb.hitbox.intersects(e.rect)) {
+                e.isvisible = false;
+
+            }
+        }
+
+        for (Itemwin w : stuffwin) {
+
+            if (player.rect.intersects(w.hitbox)) {
+                if (w.isvisible) {
+                    w.isvisible = false;
+                    makevisible();
+                    sbg.enterState(3, new FadeOutTransition(Color.black), new FadeInTransition(Color.black));
+
+                }
+
             }
         }
 
         for (Item1 h : stuff1) {
 
-            if (Player.rect.intersects(h.hitbox)) {
+            if (player.rect.intersects(h.hitbox)) {
 
                 if (h.isvisible) {
 
-                    Player.speed += .1f;
+                    player.speed += .1f;
                     h.isvisible = false;
                 }
 
             }
         }
-         if (Player.rect.intersects(gold.hitbox)){
-             gold.isvisible = false;
-         }
+        for (Enemy e : enemiez) {
+           if (player.rect.intersects(e.rect)) {
+               player.health-=400;
+               e.isvisible=false;
+               player.score+= 4;
+           }
+            e.move();
+        }
+        if (player.rect.intersects(gold.hitbox)) {
+            gold.isvisible = false;
+            
+        }
+        g.drawString("speed: " + (int) (player.speed * 10), camera.cameraX + 10,
+                camera.cameraY + 25);
 
-    
-
-
-        if (Player.health <= 0) {
+        if (player.health <= 0) {
             makevisible();
             sbg.enterState(2, new FadeOutTransition(Color.black), new FadeInTransition(Color.black));
         }
-        if(orb.getTimeExists() > 0){
-    if(Player.getdirection()==0){
-    orb.setX((int)Player.x);
-    orb.setY((int)orb.getY()+ 5);
-    
-    
-    }else if (Player.getdirection()==2){
-        
-    }
-    }
+        if (orb.getTimeExists() > 0) {
+            if (player.getdirection() == 0) {
+                orb.setX((int) player.x);
+                orb.setY((int) orb.getY() + 5);
+
+            } else if (player.getdirection() == 2) {
+
+            }
+        }
     }
 
     public int getID() {
@@ -549,7 +438,6 @@ for (Enemy e : enemiez){
         return 1;
 
     }
-
 
     public void makevisible() {
         for (Item1 h : stuff1) {
@@ -571,9 +459,6 @@ for (Enemy e : enemiez){
 
         return Blocked.blocked[xBlock][yBlock];
 
-
     }
 
 }
-
-
